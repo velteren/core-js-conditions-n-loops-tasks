6e00c566eb56915241f93f1bead447cfff2ab7d1
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-05 12:16:50 +0300

    feat: add isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -231,8 +231,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversedStr += str[i];
+  }
+  return str === reversedStr;
 }
 
 /**
