a20dae249b619636a313cc4a001dc52740792df2
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-05 12:22:37 +0300

    feat: add isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -275,8 +275,12 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = `${num}`;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === `${digit}`) return true;
+  }
+  return false;
 }
 
 /**
